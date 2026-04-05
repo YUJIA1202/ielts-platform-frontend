162d07482f9b1029cae48fef0040b1420656ad1a
YUJIA1202 <yujialuo038@gmail.com>
2026-04-06 05:30:11 +0800

    fix: hardcode api url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api',
+ baseURL: 'https://ielts-platform-backend-production-06dd.up.railway.app/api',
 })
 
 api.interceptors.request.use((config) => {
